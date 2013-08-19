3a77a305c4d7544b711eb55b112064eb721a3e9c
Adam Lindberg <hello@alind.io>
2013-08-19 21:28:11 +0200

    Add each/2

--- lib/parallel.ex
+++ lib/parallel.ex
@@ -1,52 +1,53 @@
 defmodule Parallel do
 
   def map(collection, fun, options // []) do
-    len = Enumerable.count(collection)
-    size = Keyword.get(options, :size) || if(len < 10, do: len, else: 10)
-    free = pool(size)
-    acc = {fun, free, [], []}
+    acc = {fun, pool(fun, options), [], []}
     {_, _, busy, acc} = Enumerable.reduce(collection, acc, function(execute/2) )
     List.concat(acc, consume(busy))
   end
 
+  def each(collection, fun, options // []) do
+    acc = {fun, pool(fun, options), [], []}
+    Enumerable.reduce(collection, acc, function(execute/2) )
+    nil
+ end
+
   # Private
 
-  defp execute(item, {func, free = [], busy, acc}) do
+  defp execute(item, {fun, free = [], busy, acc}) do
     receive do
-      {from, result} ->
-        from <- {self(), func, item}
-        {func, free, busy, [result|acc]}
+      {ref, from, result} ->
+        from <- {ref, self(), fun, item}
+        {fun, free, busy, [result|acc]}
     end
   end
-  defp execute(item, {func, [pid|free], busy, acc}) do
-    pid <- {self(), func, item}
-    {func, free, [pid|busy], acc}
+  defp execute(item, {fun, [worker = {ref, pid}|free], busy, acc}) do
+    pid <- {ref, self(), fun, item}
+    {fun, free, [worker|busy], acc}
   end
 
   defp consume(pool) do
-    Enum.map(pool, fn pid ->
+    Enum.map(pool, fn {ref, pid} ->
       receive do
-        {^pid, result} -> result
+        {^ref, ^pid, result} -> result
       end
     end)
   end
 
-  defp worker do
+  def worker(fun) do
     receive do
-      {sender, func, item} when is_function(func) ->
-        sender <- {self, func.(item)}
-        worker
+      {ref, sender, fun, item} when is_function(fun) ->
+        sender <- {ref, self, fun.(item)}
+        worker(fun)
       :exit ->
         :ok
     end
   end
 
-  defp pool(size) do
-    Stream.repeatedly(function(spawn_worker/0)) |> Enum.take(size)
-  end
-
-  defp spawn_worker do
-    spawn_link(function(worker/0))
+  defp pool(fun, options) do
+    size = Keyword.get(options, :size) || :erlang.system_info(:schedulers)
+    spawn_worker = fn -> {make_ref(), spawn_link(fn -> worker(fun) end)} end
+    Stream.repeatedly(spawn_worker) |> Enum.take(size)
   end
 
 end
--- test/parallel_test.exs
+++ test/parallel_test.exs
@@ -17,6 +17,20 @@ defmodule ParallelTest do
     end
   end
 
+  test :each do
+    IO.puts "each"
+    myself = self()
+    collection = [1,2,3,4,5]
+    each(collection, fn i -> myself <- {:test, i} end)
+    Enum.each(collection, fn i ->
+      receive do
+        {:test, i} -> :ok
+      after 100 ->
+        assert false, "No result received"
+      end
+    end)
+  end
+
   defp assert_map(list) do
     func = fn(x) -> x + 1 end
     assert Enum.sort(Enum.map(list, func)) == Enum.sort(map(list, func))
